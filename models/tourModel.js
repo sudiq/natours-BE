fb2e4bd345845ac87dfafa92486c196f6031116a
Sadiq Aderinto <sa.ayinda@gmail.com>
2021-03-09 17:23:58 +0100

    added validators

--- models/tourModel.js
+++ models/tourModel.js
@@ -30,7 +30,7 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       default: 4.5,
       min: [1, 'rating must be above 1.0'],
-      max: [1, 'rating must be below 5.0'],
+      max: [5, 'rating must be below 5.0'],
     },
     ratingsQuantity: { type: Number, default: 0 },
     price: { type: Number, required: [true, 'A tour must have a price'] },
