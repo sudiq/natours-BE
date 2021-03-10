7c20c224c88e60d14991c3ee6ac7f7068de93445
Sadiq Aderinto <sa.ayinda@gmail.com>
2021-03-10 06:43:34 +0100

    added debuging tool

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+// const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,10 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true,
       trim: true,
+      // validate: {
+      //   validator: validator.isAlpha,
+      //   message: 'name must be an alphabet',
+      // },
     },
     slug: String,
     duration: {
@@ -34,7 +39,15 @@ const tourSchema = new mongoose.Schema(
     },
     ratingsQuantity: { type: Number, default: 0 },
     price: { type: Number, required: [true, 'A tour must have a price'] },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          return val < this.price;
+        },
+        message: 'Discount should be below the regular price',
+      },
+    },
     summary: {
       type: String,
       trim: true,
